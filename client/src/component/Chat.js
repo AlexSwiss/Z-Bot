e6032244f0a680e17933a839317518f5e6a30c55
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-09-28 01:08:39 +0100

    resolved bugs

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -47,7 +47,7 @@ class Chat extends Component {
       conversation: [...this.state.conversation, msg],
     });
 
-    fetch('http://localhost:5000/chat', {
+    fetch('https://z-b.herokuapp.com/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
